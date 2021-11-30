cddaca8173b3be634d6dfa75d8791af49e3fe64c
Martí Paulet López <marti@localhost.localdomain>
2021-11-30 03:54:27 +0100

    setup_v1

--- AISonic_v1.cc
+++ AISonic_v1.cc
@@ -49,13 +49,6 @@ struct PLAYER_NAME : public Player {
 
 
   Dir find_next_move(Pos &a, int d, VVI &VP) {
-    /*
-    cerr << "actual: " << VP[a.i][a.j] << endl; 
-    cerr << "arriba: " << VP[(a.i)-1][a.j]<< endl;
-    cerr << "abajo: " << VP[(a.i)+1][a.j]<< endl;
-    cerr << "izquierda: " << VP[(a.i)][(a.j)-1]<< endl;
-    cerr << "derecha: " << VP[(a.i)][(a.j)+1]<< endl;
-  */
     if (pos_ok((a.i)-1,a.j) and VP[(a.i)-1][a.j] == d) {        //adalt
       a += TOP;
       return BOTTOM;
@@ -82,9 +75,7 @@ struct PLAYER_NAME : public Player {
     queue <pair<pair<int, int>, int> > q;                                       //queue with pos(i,j) and distance d 
     int movx[4] = {-1,1,0,0};                                                   //aux vectors
     int movy[4] = {0,0,-1,1};
-    Unit u = unit(id);
-    //cerr << "(" << u.pos.i <<","<< u.pos.j<< ") "; 
-    //VP[u.pos.i][u.pos.j] = 0;                                                 
+    Unit u = unit(id);                                                
     q.push(make_pair(make_pair(u.pos.i,u.pos.j),0)); 
     int x,y,d;
     bool f = true;
@@ -109,46 +100,22 @@ struct PLAYER_NAME : public Player {
               if ((aux != -1 and city_owner(aux)!= me()) or (aux2 != -1 and path_owner(aux2) != me())) {
                 Pos dest; dest.i = x+movx[i]; dest.j = y+movy[i];                        //posicio final
                 Dir next_move = NONE;
-                //IMPRIMIR MATRIU
-                /*if (f) {
-                  for (int i = 0; i < rows(); ++i) {
-                    cerr << endl;
-                    for (int j = 0; j < cols(); ++j) {
-                      cerr << VP[i][j] << " ";
-                    }
-                  }
-                  f = false;
-                }*/
-                
                 int aux = d;
                 ++d;
-                //cerr << "(" << dest.i <<"," << dest.j << ") a distancia " << d <<  endl;
-
                 for (int i = 0; i < d; ++i) {
-                  //cerr << "steps remaining " << aux << endl;
                   next_move = find_next_move(dest,aux,VP);
-                  //cerr << dest.i << " " << dest.j << endl;
                   --aux;
                 }     
-
                 move(id,next_move);                                                             
                 return;
-
               }
-
             }
-
           q.push(make_pair(make_pair(x+movx[i],y+movy[i]),d+1));
           }
-
         }
-
       }
-
     }
-
     return;   //error return
-
   }
 
 
@@ -156,13 +123,13 @@ struct PLAYER_NAME : public Player {
    * Play method, invoked once per each round.
    */
   virtual void play () {
-    //if (round() == 199) {
+    if (round() == 199) {
       vector<int> units_id = my_units(me());
       int n_units = units_id.size();
       for (int i = 0; i < n_units; ++i) {
-        /*if (i == 0)*/ bfs_units(units_id[i]);
+        bfs_units(units_id[i]);
       }
-    //}
+    }
   }
 };
 
